27c820f5ef8617da9efff26f82d0d5f1fa381661
Arjen Mourik <arjen.mourik@live.nl>
2015-12-14 11:02:16 +0100

    error handler geadd

--- Breda Ontdekt/View/Pages/MainPage.xaml.cs
+++ Breda Ontdekt/View/Pages/MainPage.xaml.cs
@@ -12,6 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Breda_Ontdekt.ViewModel.Lib;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -25,6 +26,11 @@ namespace Breda_Ontdekt.View.Pages
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            ErrorHandler.handleError("test", "");
         }
     }
 }
