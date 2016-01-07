31da5cc67bd9832c31c7be9400b287d1b39f9d24
Davey Mathijssen <daveym14@gmail.com>
2016-01-08 00:44:52 +0100

    added zoom in helppage + typo fixes and grammar in helppage

--- Breda Ontdekt/View/Pages/HelpPage.xaml.cs
+++ Breda Ontdekt/View/Pages/HelpPage.xaml.cs
@@ -27,10 +27,26 @@ namespace Breda_Ontdekt.View.Pages
     {
 
         private TransferClass transfer;
+        private int headerFont;
+        private int textFont;
+        private double margin;
 
         public HelpPage()
         {
             this.InitializeComponent();
+            if (!ViewModel.AppGlobal.ZoomedIn)
+            {
+                headerFont = 14;
+                textFont = 12;
+                margin = 18;
+            }
+            else
+            {
+                headerFont = 26;
+                textFont = 24;
+                margin = 30;
+            }
+            setSizes();
         }
 
         protected override void OnNavigatedTo(NavigationEventArgs e)
@@ -54,6 +70,58 @@ namespace Breda_Ontdekt.View.Pages
             currentView.AppViewBackButtonVisibility = AppViewBackButtonVisibility.Visible;
 
             currentView.BackRequested -= backButton_Tapped;
+        }
+
+        private void setSizes()
+        {
+            header1.FontSize = headerFont;
+            header2.FontSize = headerFont;
+            header3.FontSize = headerFont;
+            header4.FontSize = headerFont;
+            header5.FontSize = headerFont;
+            header6.FontSize = headerFont;
+            header7.FontSize = headerFont;
+            header8.FontSize = headerFont;
+            header9.FontSize = headerFont;
+            text1.FontSize = textFont;
+            text1.Margin = new Thickness(0, margin, 0, 0);
+            text2.FontSize = textFont;
+            text2.Margin = new Thickness(0, margin, 0, 0);
+            text3.FontSize = textFont;
+            text3.Margin = new Thickness(0, margin, 0, 0);
+            text4.FontSize = textFont;
+            text4.Margin = new Thickness(0, margin, 0, 0);
+            text5.FontSize = textFont;
+            text5.Margin = new Thickness(0, margin, 0, 0);
+            text6.FontSize = textFont;
+            text6.Margin = new Thickness(0, margin, 0, 0);
+            text7.FontSize = textFont;
+            text7.Margin = new Thickness(0, margin, 0, 0);
+            text8.FontSize = textFont;
+            text8.Margin = new Thickness(0, margin, 0, 0);
+            text9.FontSize = textFont;
+            text9.Margin = new Thickness(0, margin, 0, 0);
+        }
+
+        private void ZoomButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (ViewModel.AppGlobal.ZoomedIn)
+            {
+                //siteInfo.FontSize = 18;
+                headerFont = 14;
+                textFont = 12;
+                margin = 18;
+                ViewModel.AppGlobal.ZoomedIn = false;
+            }
+            else
+            {
+                //siteInfo.FontSize = 40;
+                headerFont = 26;
+                textFont = 24;
+                margin = 30;
+                ViewModel.AppGlobal.ZoomedIn = true;
+            }
+            setSizes();
         }
     }
 }
